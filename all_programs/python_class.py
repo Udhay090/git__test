c5a263fa572d4e05ee7cdc12270985db443840bd
Udhay Adbss <udhayadbss75@gmail.com>
2026-03-13 02:33:07 +0530

    added notebooks

---
+++ all_programs/python_class.py
@@ -0,0 +1,16 @@
+s="python"
+y="a"+"b"
+print("ha"*3)
+print(y)
+
+ 
+for x in [1,2,3,]:#iterating a list
+ print(x)
+for ch in "python":
+ print(ch)
+for i in range(5):#0,1,2,3,4
+   print(i)
+count = 1
+while count <= 5:
+  print(count)
+  count += 1
